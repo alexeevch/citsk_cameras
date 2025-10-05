d17b03bd76b7b1a5b192d8538094089b09345656
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-05 21:32:18 +0300

    feat(backend): Добавлен префикс маршруты для sanctum csrf token

--- backend/src/config/sanctum.php
+++ backend/src/config/sanctum.php
@@ -19,8 +19,10 @@ return [
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+    // Sanctum::currentRequestHost(),
     ))),
+
+    'prefix' => 'api/csrf',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,8 +79,8 @@ return [
 
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'encrypt_cookies'      => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token'  => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
