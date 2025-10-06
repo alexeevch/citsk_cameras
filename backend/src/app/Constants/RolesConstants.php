463a5b3ad73359de3268cd707c99fb91407ac5d5
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-06 14:14:43 +0300

    feat(backend): Добавлены константы ролей

---
+++ backend/src/app/Constants/RolesConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class RolesConstants
+{
+    public const string ROLE_ROOT = 'Root';
+    public const string ROLE_ADMIN = 'Admin';
+    public const string ROLE_USER = 'User';
+}
