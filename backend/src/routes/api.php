6d6db0f2305bf74246720bf90fcf614fce3bdbb2
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-07 10:42:48 +0300

    fix: Обновлены middlewares маршрутов api

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->middleware("web");
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:web');
-    Route::get('me', [AuthController::class, 'me'])->middleware('auth:web');
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 });
