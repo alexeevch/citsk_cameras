49d5e9ea3797ea98791b4adc7cdab9921c074026
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-10-07 11:07:51 +0300

    feat(frontend): Выключен ssr в конфиге nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   modules: ["@nuxt/eslint", "@pinia/nuxt"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
