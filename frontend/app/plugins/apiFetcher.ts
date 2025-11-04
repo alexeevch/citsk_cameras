c67da4165b9379d08e93ccda28998219a4ecd80c
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-11-04 17:31:04 +0300

    feat(frontend): Базовый плагин для запросов к api

--- frontend/app/plugins/apiFetcher.ts
+++ frontend/app/plugins/apiFetcher.ts
@@ -3,6 +3,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const UNAUTHORIZED_ERROR_CODE = 401;
 
   const baseURL = useRuntimeConfig().public.apiUrl;
+  const xsrfToken = useCookie("XSRF-TOKEN").value;
 
   const apiFetcher = $fetch.create({
     baseURL,
@@ -10,9 +11,19 @@ export default defineNuxtPlugin((nuxtApp) => {
     credentials: "include",
     onRequest(request) {
       request.options.headers.set("Accept", "application/json");
+
+      if (xsrfToken) {
+        request.options.headers.set(
+          "X-XSRF-TOKEN",
+          decodeURIComponent(xsrfToken),
+        );
+      }
     },
     async onResponseError({ response }) {
-      if (response.status === UNAUTHORIZED_ERROR_CODE) {
+      if (
+        response.status === UNAUTHORIZED_ERROR_CODE ||
+        response.status === CSRF_ERROR_CODE
+      ) {
         await nuxtApp.runWithContext(() => navigateTo("/auth/login"));
       }
     },
