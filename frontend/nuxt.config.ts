76d8a34d5b03aa422cad19a2b547635626afda27
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-11-04 17:32:21 +0300

    feat(frontend): Добавлен primevue

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,18 +1,35 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    renderJsonPayloads: false,
+  },
   ssr: false,
 
   runtimeConfig: {
     public: {
+      appName: import.meta.env.NUXT_APP_NAME ?? "",
       apiUrl: import.meta.env.NUXT_API_URL ?? "",
     },
   },
 
-  modules: ["@nuxt/eslint", "@pinia/nuxt"],
+  modules: ["@nuxt/eslint", "@pinia/nuxt", "@primevue/nuxt-module"],
+
+  primevue: {
+    options: {
+      theme: {
+        theme: "none",
+      },
+    },
+    components: {
+      exclude: ["Editor", "Chart"],
+    },
+  },
 
   pinia: {
     storesDirs: [".app/stores/**"],
   },
+
+  css: ["~/assets/css/style.css"],
 
   compatibilityDate: "2025-07-15",
 });
