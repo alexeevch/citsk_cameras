1d12925c87bca5c013cee3386805e246f866107b
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2025-11-05 11:25:52 +0300

    fix(frontend): Обновлен apiFetcher

--- frontend/app/plugins/apiFetcher.client.ts
+++ frontend/app/plugins/apiFetcher.client.ts
@@ -3,7 +3,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   const UNAUTHORIZED_ERROR_CODE = 401;
 
   const baseURL = useRuntimeConfig().public.apiUrl;
-  const xsrfToken = useCookie("XSRF-TOKEN").value;
 
   const apiFetcher = $fetch.create({
     baseURL,
@@ -11,18 +10,16 @@ export default defineNuxtPlugin((nuxtApp) => {
     credentials: "include",
     onRequest(request) {
       request.options.headers.set("Accept", "application/json");
+      const xsrfToken = useCookie("XSRF-TOKEN");
 
       if (xsrfToken) {
-        request.options.headers.set(
-          "X-XSRF-TOKEN",
-          decodeURIComponent(xsrfToken),
-        );
+        request.options.headers.set("X-XSRF-TOKEN", xsrfToken.value!);
       }
     },
     async onResponseError({ response }) {
       if (
-        response.status === UNAUTHORIZED_ERROR_CODE ||
-        response.status === CSRF_ERROR_CODE
+        response.status === CSRF_ERROR_CODE ||
+        response.status === UNAUTHORIZED_ERROR_CODE
       ) {
         await nuxtApp.runWithContext(() => navigateTo("/auth/login"));
       }
